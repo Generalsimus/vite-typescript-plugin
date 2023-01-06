17529685f19c65e6666af5b58e5c396e4b343610
Generalsimus <totorotomoney@gmail.com>
2023-01-06 18:32:44 +0400

    update

--- host/readFile.ts
+++ host/readFile.ts
@@ -2,9 +2,9 @@ import ts from "typescript"
 import { CustomCompilerHost } from "./";
 
 
-export function readFile(this: CustomCompilerHost, fileName: string) {
+export function readFile(this: CustomCompilerHost, fileName: string, encoding?: string) {
     const fileDetails = this.getCacheFileDetails(fileName);
 
 
-    return (fileDetails.sourceFile?.text || fileDetails.code) || (fileDetails.code = ts.sys.readFile(fileName, this.configFileOptions.options.charset))
+    return (fileDetails.sourceFile?.text || fileDetails.code) || (fileDetails.code = ts.sys.readFile(fileName, this.configFileOptions.options.charset || encoding))
 }
